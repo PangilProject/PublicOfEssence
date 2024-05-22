e7365e6f12571730989f59f11638535ae28d51db
pangil5634 <oksk6681@naver.com>
2024-05-22 23:20:10 +0900

    Edit : stop music function and button

--- src/Components/AutoPlayAudio.js
+++ src/Components/AutoPlayAudio.js
@@ -1,59 +1,47 @@
-// import React, { useEffect, useRef } from 'react';
-
-// function AutoPlayAudio() {
-//     const audioRef = useRef(null);
-
-//     useEffect(() => {
-//         // 컴포넌트가 마운트된 후 오디오 자동 재생
-//         audioRef.current.play().catch(error => {
-//             // 재생에 실패할 경우 (예: 브라우저의 자동 재생 정책)
-//             console.log('Audio play failed:', error);
-//         });
-//     }, []);
-
-//     return (
-//         <div>
-//             <audio ref={audioRef} src="/audio/myBgm.mp3" controls />
-//             {/* <audio ref={audioRef} src="/audio/myBgm.mp3"/> */}
-//         </div>
-//     );
-// }
-
-// export default AutoPlayAudio;
-
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 function AutoPlayAudio() {
     const audioRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     const handlePlayAudio = () => {
         audioRef.current.play().catch(error => {
-            console.log('Audio play failed:', error);
+        console.log('Audio play failed:', error);
         });
+        setIsPlaying(true);
+    };
+
+    const handleStopAudio = () => {
+        audioRef.current.pause();
+        setIsPlaying(false);
     };
 
     return (
         <div>
+        {isPlaying ? (
+            <StopButton onClick={handleStopAudio}>중단 하기</StopButton>
+        ) : (
             <PlayButton onClick={handlePlayAudio}>음악 틀기</PlayButton>
-            <audio ref={audioRef} src="/audio/myBgm.mp3" loop/>
+        )}
+        <audio ref={audioRef} src="/audio/myBgm.mp3" loop />
         </div>
     );
 }
 
-const PlayButton = styled.button`
-    padding : 5px 10px;
-
+const Button = styled.button`
+    padding: 5px 10px;
     border-radius: 10px;
-
     background-color: white;
-    border : 1px solid #333;
-
-    &:hover{
+    border: 1px solid #333;
+    &:hover {
         background-color: #333;
-        color : white;
+        color: white;
     }
     margin-bottom: 30px;
 `;
+
+const PlayButton = styled(Button)``;
+const StopButton = styled(Button)``;
 
 export default AutoPlayAudio;
