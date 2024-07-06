af6668b84e3180c2f91c2899c1dd00c51b4c63dc
pangil5634 <oksk6681@naver.com>
2024-07-07 01:36:34 +0900

    edit : MyRoutes code align

--- src/Layout/MyRoutes.js
+++ src/Layout/MyRoutes.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import HomePage from "../Pages/HomePage/HomePage"
 import WorshipPage from "../Pages/WorshipPage/WorshipPage"
 import LayoutOutlet from "./LayoutOutlet";
@@ -6,15 +6,15 @@ import AboutPage from "../Pages/AboutPage/AboutPage";
 import ContactPage from "../Pages/ContactPage/ContactPage";
 import SchedulePage from "../Pages/SchedulePage/SchedulePage";
 function MyRoutes() {
-    
+
     return (
         <Routes>
             <Route element={<LayoutOutlet />}>
-                <Route index path="/" element={<HomePage />} />
-                <Route path = "/about" element={<AboutPage/>} />
-                <Route path="/worship" element={<WorshipPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path = "/schedule" element={<SchedulePage/>}/>
+                <Route index="index" path="/" element={<HomePage />}/>
+                <Route path="/about" element={<AboutPage/>}/>
+                <Route path="/worship" element={<WorshipPage />}/>
+                <Route path="/contact" element={<ContactPage />}/>
+                <Route path="/schedule" element={<SchedulePage/>}/>
             </Route>
         </Routes>
     );
