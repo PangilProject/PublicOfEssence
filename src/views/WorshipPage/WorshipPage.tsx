49e170f8c1e54aa148c343a5bbb0a0246e96d3d4
김광일 <54162245+pangil5634@users.noreply.github.com>
2026-07-14 23:16:30 +0900

    feat: show worship list in latest-first order
    집회 목록을 회차 내림차순으로 정렬해 최신 집회가 먼저 보이도록 변경했습니다.
    원본 데이터는 유지하고 렌더링 시에만 정렬하며, key를 index 대신 회차 번호로 교체했습니다.

--- src/views/WorshipPage/WorshipPage.tsx
+++ src/views/WorshipPage/WorshipPage.tsx
@@ -7,12 +7,15 @@ import WorshipItem from "./components/WorshipItem";
 import { ContainerWorshipList } from "./styles";
 
 function WorshipPage() {
+  /* 최신 집회가 먼저 보이도록 회차 내림차순 정렬 */
+  const sortedList = [...worshipList].sort((a, b) => b.count - a.count);
+
   return (
     <Container100P100P>
       <PageHero kicker="WORSHIP" title="2016년부터 이어온 집회의 기록" />
       <ContainerWorshipList>
-        {worshipList.map((item, index) => (
-          <WorshipItem key={index} item={item} />
+        {sortedList.map((item) => (
+          <WorshipItem key={item.count} item={item} />
         ))}
       </ContainerWorshipList>
     </Container100P100P>
